ea12f015125bde521b41442e3afa3da06eed75bd
seyeoyadiran <seyeoyadiran@gmail.com>
2024-12-17 00:04:21 -0500

    Adding Styling for the chat component

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,64 @@
 import "./chatList.css";
+import Image1 from '../imgs/search1.png';
+import Plus from '../imgs/plus.png';
+import Minus from '../imgs/Minus.png';
+import Avatar from '../imgs/ava.png';
+import { useState } from "react";
 
 const ChatList = () => {
+    
+    const [addMode, setAddMode ] = useState(false);
+ 
     return (
-        <div className="chatList">ChatList</div>
+        <div className="chatList">
+            <div className="search">
+                <div className="searchBar">
+                    <img src={Image1} alt=""/>
+                    <input type="text" placeholder="Search"/>
+                </div>
+                <img src={addMode ? Minus : Plus} alt="" className="add"
+                onClick={()=> setAddMode((prev) => !prev)}/>
+            </div>
+            <div className="item">
+                <img src={Avatar} alt=""/>
+                <div className="texts">
+                    <span> John O</span>
+                    <p> Hello </p>
+                </div>  
+            </div>
+            <div className="item">
+                <img src={Avatar} alt="" />
+                <div className="texts">
+                    <span> John O</span>
+                    <p> Hello </p>
+                </div>  
+            </div>
+            <div className="item">
+                <img src={Avatar} alt="" />
+                <div className="texts">
+                    <span> John O</span>
+                    <p> Hello </p>
+                </div>  
+            </div>
+            <div className="item">
+                <img src={Avatar} alt="" />
+                <div className="texts">
+                    <span> John O</span>
+                    <p> Hello </p>
+                </div>  
+            </div>
+            <div className="item">
+                <img src={Avatar} alt="" />
+                <div className="texts">
+                    <span> John O</span>
+                    <p> Hello </p>
+                </div>  
+            </div>
+            
+        </div>
+            
+
+            
     )
 }
 
--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -1,22 +1,23 @@
 import "./userInfo.css";
 import Image from '../imgs/ava.png';
 import Image2 from '../imgs/more.png';
-import Image3 from '../imgs/FaceTime.webp';
-import Image4 from '../imgs/editpic.png';
+import Image3 from '../imgs/FT.png';
+import Image4 from '../imgs/edit.png';
 
 const UserInfo = () => {
 
     return(
         <>
-          <div className='userInfo'> User Info </div>
-            <div className='user'> 
-                <img src={Image} alt="" />
+          <div className='userInfo'> 
+          <div className='user'> 
+                <img src={Image} alt=""/>
                 <h2> Oluwaseye Oyadiran </h2>    
             </div>  
             <div className='icons'>  
                 <img src={Image2} alt="" />
                 <img src={Image3} alt=""/>
                 <img src={Image4} alt=""/>
+            </div>
             </div>
         </>  
     )
